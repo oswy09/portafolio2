6f9a9c113b0b6fa5ccf8f4c181d36147739f79e4
oswy09 <142705793+oswy09@users.noreply.github.com>
2025-08-11 17:05:04 -0500

    Updated ProjectDetail.vue

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -505,7 +505,7 @@ const projects: Project[] = [
       ],
     },
     solution: {
-      gif: 
+      gif: ',
       description:
         'Se entregó una aplicación web interactiva de quizz construida desde cero con Vue.js Esta interfaz no solo facilita el estudio personalizado por temas y la autoevaluación para los concursos de ascenso, sino que también ofrece un panel administrativo intuitivo para la gestión total de usuarios y contenido por parte del Acueducto de Bogotá.',
     },
