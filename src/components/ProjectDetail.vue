c745ab45206c471abb1a5b3a07cda7bfefa2e3aa
oswy09 <142705793+oswy09@users.noreply.github.com>
2025-08-26 16:00:05 -0500

    Updated ProjectDetail.vue

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -758,7 +758,7 @@ const projects: Project[] = [
     industry: 'Centro comercial',
     location: 'Bogotá, Colombia',
     myRole: 'Web designer',
-    responsibilities: ['UX design', 'UI Design', 'Frontend Developer'],
+    responsibilities: ['Web designer', 'Frontend Developer'],
     challenge: 'MiCentro, un centro comercial en crecimiento, necesitaba establecer una presencia online sólida y atractiva que reflejara su ambiente dinámico y facilitara la conexión entre visitantes y sus diversos negocios. El desafío radicaba en crear un sitio web fácil de gestionar por el cliente, que sirviera como directorio de marcas y eventos, potenciara la visibilidad orgánica y ofreciera una experiencia de usuario que invitara a la interacción, superando la falta de un canal digital unificado y optimizado.',
     process: {
       research: [
