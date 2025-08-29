a46d94edb6e3307c264d93370c6d8cf74363611e
oswy09 <142705793+oswy09@users.noreply.github.com>
2025-08-29 18:57:32 -0500

    Updated ProjectDetail.vue

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -1194,6 +1194,21 @@ imageGallery: [
       { value: '+40%', metric: 'Facilidad de Gestión de Contenido' },
     ],
     liveUrl: 'https://www.amazonbb.com/',
+     imageGallery: [
+      {
+        url: "https://res.cloudinary.com/ddqbnr9vo/image/upload/v1756511734/Proyectos_oswal_web_design_AXA_Home_page_p62gsa.jpg",
+        alt: "Imagen resultado sitio web_AXA_COLPATRIA",
+        caption: "Home AXA Colpatria",
+        tag: "resultado" 
+      },
+      {
+        url: "https://oswal.com.co/wp-content/uploads/2025/08/Amazon_mockup03-scaled.jpg",
+        alt: "Imagen resultado sitio web_Amazon b&B",
+        caption: "Hotel amazon B&B_portafolio servicios",
+        tag: "resultado" 
+      }
+     
+    ]
   },
   {
     id: 9,
