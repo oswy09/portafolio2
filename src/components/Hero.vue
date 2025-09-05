45ebcb9bd7bd23a0f1b9e0e3c4fd89e7081903e6
oswy09 <142705793+oswy09@users.noreply.github.com>
2025-09-05 15:24:14 -0500

    Updated Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -52,7 +52,7 @@
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
                 <polyline points="22,6 12,13 2,6"/>
               </svg>
-              contact@oswal.com
+              contacto@oswal.com
             </a>
             <div class="social-links">
               <a href="#" class="social-link">
